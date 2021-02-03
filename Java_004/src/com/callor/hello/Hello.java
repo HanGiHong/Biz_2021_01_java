3cc3ad792e4c6c497c007d5335fb98712274e324
HanGiHong <dnjfdkch@naver.com>
2021-02-03 09:31:30 +0900

    REAME 수정

--- Java_004/src/com/callor/hello/Hello.java
+++ Java_004/src/com/callor/hello/Hello.java
@@ -1,5 +1,23 @@
 package com.callor.hello;
 
 public class Hello {
+	
+	public static void main(String[] args) {
+		
+		System.out.println( 30 + 40 );
+		System.out.println( 30 * 40 );
+		
+		
+		int nm1 = 55; 
+		int nm2 = 68;
+		
+		
+		System.out.println(nm1 + nm2);
+		System.out.println(nm1 * nm2);
+		System.out.println(nm2 - nm1);
+		System.out.println(nm2 / nm1);
+		System.out.println(nm2 % nm2);
+		
+	}
 
 }
