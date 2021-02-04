d547ef7ccde523fc1a665ac1a1d51f9e4b10d09b
HanGiHong <dnjfdkch@naver.com>
2021-02-04 16:11:27 +0900

    반복문

---
+++ Java_006/src/com/callor/varriable/Loop_02.java
@@ -0,0 +1,22 @@
+package com.callor.varriable;
+
+public class Loop_02 {
+	
+	public static void main(String[] args) {
+		
+		int num1 = 0;
+		for( ;;) {
+			
+			/*
+			 * 정수형변수 nim1의 값을
+			 * 1씩 증가시키고(1,2,3,4,5 ~~~~)
+			 * 그 값을 Console에 출력하라
+			 */
+			num1 += 1;
+			System.out.println(num1);
+			
+			
+		}
+	}
+
+}
